0dc1bf54cbf04e49a820de1ec833e7bed37860d9
Gleen Ryan <tjiagleenryan@gmail.com>
2026-06-29 14:38:25 +0800

    Initial commit

---
+++ Challenge4/Challenge4App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge4App.swift
+//  Challenge4
+//
+//  Created by Gleenryan on 29/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Challenge4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge4
+//
+//  Created by Gleenryan on 29/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
