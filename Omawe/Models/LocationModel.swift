90c8cc3ce0d02ba59e702df31954b3100d5575d4
Kahfi R <110613182+nrkahfirhmd@users.noreply.github.com>
2026-06-30 11:30:46 +0800

    add: data models

--- Omawe/Models/LocationModel.swift
+++ Omawe/Models/LocationModel.swift
@@ -7,12 +7,10 @@
 
 import SwiftUI
 
-struct LocationModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    LocationModel()
+struct LocationModel {
+    let id = UUID()
+    var tripCode: String
+    var userId: UUID
+    var location: Location
+    var lastUpdated: Date
 }
---
+++ Omawe/Models/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  Omawe
+//
+//  Created by Nurkahfi Rahmada on 30/06/26.
+//
+
+import Foundation
+
+struct UserModel {
+    let id = UUID()
+    var name: String
+    var profilePicture: Data?
+}
