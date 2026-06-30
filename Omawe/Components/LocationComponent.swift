17ced2b310ec3515437bbdf8688a3f2495b9fd62
Muhammad Bintang Al-Fath <alfathbintangmuhammad@gmail.com>
2026-06-30 08:31:41 +0800

    chore: restructuring project folder

---
+++ Omawe/Components/LocationComponent.swift
@@ -0,0 +1,18 @@
+//
+//  LocationComponent.swift
+//  Omawe
+//
+//  Created by Muhammad Bintang Al-Fath on 30/06/26.
+//
+
+import SwiftUI
+
+struct LocationComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationComponent()
+}
